3d11783931775325a88c75c24d9034cf7db0b30d
GarrettW7615 <garrettw7615@gmail.com>
2022-03-27 13:27:48 -0500

    Added movement scripts, test door that changes to a different scene, sprint functionality, test for animation

---
+++ Scripts/Movement.gd
@@ -0,0 +1,36 @@
+extends KinematicBody2D
+
+export (int) var speed = 200
+var velocity = Vector2()
+
+onready var anim = $PlayerAnimation
+
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed("right"):
+		if Input.is_action_pressed("Sprint"):
+			speed = 400
+		velocity.x += 1
+	if Input.is_action_pressed("left"):
+		if Input.is_action_pressed("Sprint"):
+			speed = 400
+		velocity.x -= 1
+	if Input.is_action_pressed("down"):
+		if Input.is_action_pressed("Sprint"):
+			speed = 400
+		velocity.y += 1
+	if Input.is_action_pressed("up"):
+		if Input.is_action_pressed("Sprint"):
+			speed = 400
+		velocity.y -= 1
+	velocity = velocity.normalized() * speed
+	speed = 200
+
+func _physics_process(_delta):
+	get_input()
+	velocity = move_and_slide(velocity)
+
+
+func _on_Door_body_entered(_body):
+	print("Body entered")
+	get_tree().change_scene("res://Scenes/InDoor.tscn")
