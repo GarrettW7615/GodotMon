483b925ce7717488499d8e241da7e39c7deccd26
GarrettW7615 <garrettw7615@gmail.com>
2022-03-30 00:37:11 -0500

    Started writing logic for inventory

---
+++ Scripts/Inventory.gd
@@ -0,0 +1,13 @@
+extends Control
+
+class item:
+	var item_name
+	var item_description
+	var item_type
+	var item_effect
+
+var item_list = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
